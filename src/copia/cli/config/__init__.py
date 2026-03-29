4e079e81b2cf6d370fa0ee2d8e93f1fb397b9cbf
gitmobkab <richmondaurel77@gmail.com>
2026-03-29 19:14:53 +0000

    feat: import utility functions for profile validation and config resolution

--- src/copia/cli/config/__init__.py
+++ src/copia/cli/config/__init__.py
@@ -4,3 +4,4 @@ from .exceptions import *
 from .globals import *
 from .loaders import get_profile, get_any_profile
 from .writers import save_new_profile, create_new_config_file
+from .utils import is_ascii_only, is_valid_hostname, resolve_config_path
