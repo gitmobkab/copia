e2bd4f3e882302859b9f0fa1d8ecd10796166d1e
gitmobkab <richmondaurel77@gmail.com>
2026-03-31 01:25:59 +0000

    feat: improved print_error

--- src/copia/cli/console_utils.py
+++ src/copia/cli/console_utils.py
@@ -1,31 +1,30 @@
 from typing import Any
 
-from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn
 from rich.console import Console
 from rich.panel import Panel
-from typer import Context
-
-from .config.globals import APP_NAME
 
 stderr_console = Console(stderr=True)
 stdout_console = Console()
 
-spinnerProgress = Progress(
-    SpinnerColumn(),
-    *Progress.get_default_columns(),
-    TimeElapsedColumn()
-)
 
-def print_error(error: Exception, ctx: Context | None = None, help_msg: None | str = None) -> None:
-    if ctx is not None:
-        stderr_console.print(f"[yellow]Usage:[/] {ctx.get_usage()}")
+COPIA_DOC_LINK = "https://github.com"
+SUBTITLE_HELP = f"[dim]See [blue]{COPIA_DOC_LINK}[/blue] for help."
+
+def print_error(error: Exception, help_msg: None | str = None) -> None:
     
     if help_msg is not None:
-        stderr_console.print(f"[dim]{help_msg}")
-    else:
-        stderr_console.print(f"[dim]Try [blue]'{APP_NAME} --help'[/blue] for help.")
+        stdout_console.print(f"[dim]{help_msg}")
     
-    error_panel = Panel(str(error), title="Error", border_style="red", title_align="left", highlight=True)
+    
+    error_panel = Panel(str(error),
+                        title="Error",
+                        title_align="left",
+                        subtitle=SUBTITLE_HELP,
+                        subtitle_align="left",
+                        border_style="red",
+                        highlight=True,
+                        padding=(1))
+    
     stderr_console.print(error_panel)
         
         
