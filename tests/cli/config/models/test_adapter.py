65362da66a5f90d601a7d0499f727828de1d64f1
gitmobkab <richmondaurel77@gmail.com>
2026-04-20 03:03:12 +0000

    feat: remove postgres tests and clean up test files

--- tests/cli/config/models/test_adapter.py
+++ tests/cli/config/models/test_adapter.py
@@ -9,9 +9,6 @@ class TestAdapter:
     def test_mysql(self):
         assert make_profile(adapter="mysql").adapter == "mysql"
 
-    def test_postgres(self):
-        assert make_profile(adapter="postgres").adapter == "postgres"
-
     def test_invalid_adapter(self):
         with pytest.raises(ValidationError):
             make_profile(adapter="sqlite")
