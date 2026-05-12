69598eba68f3703c786b218b6d0760112ed6fe71
gitmobkab <richmondaurel77@gmail.com>
2026-05-12 02:59:31 +0000

    tests: update tests to the new parser interface

--- tests/parser/test_named.py
+++ tests/parser/test_named.py
@@ -6,30 +6,40 @@ class TestNamed:
     def test_single_named_string(self):
         result = parse(':email(domain="gmail.com")')
         assert result == [
-            Column(None, GeneratorCall("email", Params([], {"domain": "gmail.com"})))
+            Column(None,
+                   False,
+                   GeneratorCall("email", Params([], {"domain": "gmail.com"})))
         ]
 
     def test_single_named_int(self):
         result = parse(":text(size=500)")
         assert result == [
-            Column(None, GeneratorCall("text", Params([], {"size": 500})))
+            Column(None,
+                   False,
+                   GeneratorCall("text", Params([], {"size": 500})))
         ]
 
     def test_named_bool(self):
         result = parse(":gen(safe=true)")
         assert result == [
-            Column(None, GeneratorCall("gen", Params([], {"safe": True})))
+            Column(None,
+                   False,
+                   GeneratorCall("gen", Params([], {"safe": True})))
         ]
 
     def test_multiple_named(self):
         result = parse(':email(domain="gmail.com", safe=True)')
         assert result == [
-            Column(None, GeneratorCall("email", Params([], {"domain": "gmail.com", "safe": True})))
+            Column(None,
+                   False,
+                   GeneratorCall("email", Params([], {"domain": "gmail.com", "safe": True})))
         ]
 
     def test_whitespace_around_equals(self):
         result = parse(":text( size = 500 )")
         assert result == [
-            Column(None, GeneratorCall("text", Params([], {"size": 500})))
+            Column(None,
+                   False,
+                   GeneratorCall("text", Params([], {"size": 500})))
         ]
 
