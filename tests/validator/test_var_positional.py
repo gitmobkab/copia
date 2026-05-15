98faebfb2030ca4ac1bf807be32ce88c854011be
gitmobkab <richmondaurel77@gmail.com>
2026-05-15 17:21:47 +0000

    refactor: replace VALIDATOR.validate with VALIDATOR.validate_one in test cases

--- tests/validator/test_var_positional.py
+++ tests/validator/test_var_positional.py
@@ -9,13 +9,13 @@ from copia.validator.exceptions import TypeMismatchException
 class TestVarPositional:
     def test_many_positionals_does_not_raise(self):
         call = make_call("fake_enum", positionals=["a", "b", "c", "d"])
-        VALIDATOR.validate(call)
+        VALIDATOR.validate_one(call)
 
     def test_keyword_only_after_var_positional(self):
         call = make_call("fake_racist_picker", positionals=["a", "b"], named={"racism": True})
-        VALIDATOR.validate(call)
+        VALIDATOR.validate_one(call)
 
     def test_wrong_keyword_only_type_raises(self):
         call = make_call("fake_racist_picker", positionals=["a"], named={"racism": "oops"})
         with pytest.raises(TypeMismatchException):
-            VALIDATOR.validate(call)
+            VALIDATOR.validate_one(call)
