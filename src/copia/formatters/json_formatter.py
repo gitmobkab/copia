dd7628a13878bfe82584ad64405bf36cff12f410
gitmobkab <richmondaurel77@gmail.com>
2026-05-18 00:53:38 +0000

    fix: update JSON formatting in format_to_json function

--- src/copia/formatters/json_formatter.py
+++ src/copia/formatters/json_formatter.py
@@ -1,7 +1,11 @@
 from typing import Generator
-
+from json import dumps
 from copia.runners import GeneratedRow
 
-def format_to_json(rows: list[GeneratedRow]) -> Generator[GeneratedRow, None, None]:
-    for row in rows:
-        yield row
+def format_to_json(rows: list[GeneratedRow]) -> Generator[str, None, None]:
+    yield "["
+    for index, row in enumerate(rows, 1):
+        line = "\t" + dumps(row)
+        sep = ", " if index != len(rows) else ""
+        yield line + sep
+    yield "]"
