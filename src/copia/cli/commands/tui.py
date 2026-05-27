8b85cf4b395e4b8976342995b588b5507c9c03d7
gitmobkab <richmondaurel77@gmail.com>
2026-05-27 13:07:03 +0000

    fix: enhance command descriptions and options in run and tui commands

--- src/copia/cli/commands/tui.py
+++ src/copia/cli/commands/tui.py
@@ -19,7 +19,12 @@ def main(
                                             "-l", "--local",
                                             help=f"Search only in [green]'{LOCAL_COPIA_FILE}'")
     ):
-    """Launch the interactive tui
+    """Launch the interactive tui. require a valid configuration profile to run. Use --help for more options.
+    
+    This command starts to the interactive TUI (Text User Interface) of Copia,
+    allowing you to generate data and manage your configurations in an interactive way.
+    
+    You can specify a profile to use for the session, and choose to search for configurations in either the global or local configuration files.
     """
 
     adapter = load_adapter_from_profile(profile_name, search_globals_only, search_locals_only)
