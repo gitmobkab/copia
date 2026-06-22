6ece12ddfe806e87600078e1ea475f80239ab4cc
gitmobkab <richmondaurel77@gmail.com>
2026-06-22 16:11:11 +0200

    fix: update GLOBAL_COPIA_FILE path and expand Adapter type to include 'sqlite'

--- src/copia/cli/config/globals.py
+++ src/copia/cli/config/globals.py
@@ -1,5 +1,5 @@
 """All the CONSTANTS shared by the copia config module"""
-from typing import Literal, TypeAlias
+from typing import Literal, TypeAlias, get_args
 from pathlib import Path
 
 from typer import get_app_dir
@@ -8,9 +8,10 @@ from typer import get_app_dir
 APP_NAME = "copia"
 APP_DIR = get_app_dir(APP_NAME)
 LOCAL_COPIA_FILE = Path(".copia.toml")
-GLOBAL_COPIA_FILE = Path(APP_DIR) / "profiles.toml"
+GLOBAL_COPIA_FILE = Path(APP_DIR) / "config.toml"
 PORT_MIN_VAL = 1
 PORT_MAX_VAL = 65535
-Adapter: TypeAlias = Literal['mysql', 'postgres']
+Adapter: TypeAlias = Literal['mysql', 'postgres', 'sqlite']
+Adapters: list[str] = list(get_args(Adapter))
 ConfigScope: TypeAlias = Literal["local", "global"]
 
